880443e7de14b59a41b3068131efb319bdb2b789
fnk0c <fnk0c@users.noreply.github.com>
2014-09-21 20:45:46 -0300

    Update bash_script.sh

--- bash_script.sh
+++ bash_script.sh
@@ -1,7 +1,9 @@
 #/bin/bash
 
+cd ..
+mv cangibrina /opt/
 touch /usr/bin/cangibrina
 echo \#/bin/bash >> /usr/bin/cangibrina
-echo cd /home/franco/Documentos/programação/Projetos/Cangibrina/cangibrina/ >> /usr/bin/cangibrina
+echo cd /opt/cangibrina >> /usr/bin/cangibrina
 echo exec python2 cangibrina-1.7.py \"\$\@\" >> /usr/bin/cangibrina
 chmod +x /usr/bin/cangibrina
