eb6607efd8d9bd941160ba5caaf7591d5bad3e5a
fnk0c <franco.c.colombino@gmail.com>
2014-10-26 00:58:01 -0200

    update bash script

--- bash_script.sh
+++ bash_script.sh
@@ -5,7 +5,7 @@ mv cangibrina /opt/
 touch /usr/bin/cangibrina
 echo \#/bin/bash >> /usr/bin/cangibrina
 echo cd /opt/cangibrina >> /usr/bin/cangibrina
-echo exec python2 cangibrina-1.7.py \"\$\@\" >> /usr/bin/cangibrina
+echo exec python2 cangibrina-0.8.py \"\$\@\" >> /usr/bin/cangibrina
 chmod +x /usr/bin/cangibrina
 echo Processo finalizado.
 echo cangibrina disponivel em /opt/cangibrina ou digitando cangibrina em seu terminal
