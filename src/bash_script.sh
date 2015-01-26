ae5a085a67ef4a9554448e02b1639022f78562f8
Franco Colombino <fnk0c@users.noreply.github.com>
2015-01-26 20:22:36 -0200

    Update bash_script.sh

--- src/bash_script.sh
+++ src/bash_script.sh
@@ -4,11 +4,16 @@ if [ -e /usr/bin/cangibrina ]
 then
 	rm /usr/bin/cangibrina
 	echo limpando arquivos obsoletos
-	
 fi
 
 cd ..
 cd ..
+if [ -e //opt/cangibrina ]
+then
+	rm -r /opt/cangibrina
+	echo limpando arquivos obsoletos
+fi
+
 mv cangibrina /opt/
 touch /usr/bin/cangibrina
 echo \#/bin/bash >> /usr/bin/cangibrina
