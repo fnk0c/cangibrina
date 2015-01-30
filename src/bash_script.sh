f70508d19a3e834da42671f6a336b2cc7245c7e5
Franco Colombino <fnk0c@users.noreply.github.com>
2015-01-30 13:12:49 -0200

    Update bash_script.sh

--- src/bash_script.sh
+++ src/bash_script.sh
@@ -1,24 +1,25 @@
-#/bin/bash
+#!/bin/bash
 
 if [ -e /usr/bin/cangibrina ]
 then
 	rm /usr/bin/cangibrina
-	echo limpando arquivos obsoletos
 fi
 
-cd ..
-cd ..
-if [ -e //opt/cangibrina ]
+cd .. && cd ..
+
+if [ -e /opt/cangibrina ]
 then
 	rm -r /opt/cangibrina
 	echo limpando arquivos obsoletos
 fi
 
-mv cangibrina /opt/
-touch /usr/bin/cangibrina
-echo \#/bin/bash >> /usr/bin/cangibrina
+echo \#\!/bin/bash >> /usr/bin/cangibrina
 echo cd /opt/cangibrina >> /usr/bin/cangibrina
 echo exec python2 cangibrina.py \"\$\@\" >> /usr/bin/cangibrina
+
+mv cangibrina /opt/
 chmod +x /usr/bin/cangibrina
-echo Processo finalizado.
-echo cangibrina disponivel em /opt/cangibrina ou digitando cangibrina em seu terminal
+
+echo ' [+] Pronto | Ready' .
+echo 'cangibrina disponivel em /opt/cangibrina ou digitando cangibrina em seu terminal'
+echo 'Cangibrina is available at /opt/cangibrina or typing cangibrina on your terminal'
